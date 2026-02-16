6015f73ff67fcca6e72f64463318d0cd2e5c1efd
Cyan <cyanbun96@gmail.com>
2026-02-16 23:13:29 +0200

    Fix for the previous commit

--- src/d_polyse.c
+++ src/d_polyse.c
@@ -528,7 +528,7 @@ void D_PolysetDrawSpans8Dithered(spanpackage_t *pspanpackage)
 						pix = ((u8*)acolormap)[texel + (llight & 0xFF00)];
 					else
 						pix = D_GetRGBPix(((u8*)acolormap)[texel]);
-					if (pix==0xFF) {
+					if (pix!=0xFF) {
 						if (r_alphastyle.value == 0 && cur_ent_alpha != 1) {
 							s32 curpix = *lpdest;
 							*lpdest = color_mix_lut[curpix][pix]
