129d705b4c09931dd992b238cef10c8dc7760b63
Cyan <cyanbun96@gmail.com>
2026-05-15 22:48:02 +0300

    Removed an unused function

--- src/screen.c
+++ src/screen.c
@@ -312,49 +312,6 @@ void SCR_DrawConsole()
 			Con_DrawNotify(); // only draw notify in game
 }
 
-void WritePCXfile(s8 *filename, u8 *data, s32 width, s32 height,
-		  s32 rowbytes, u8 *palette)
-{
-	pcx_t *pcx = Hunk_TempAlloc(width * height * 2 + 1000);
-	if (pcx == NULL) {
-		Con_Printf("SCR_ScreenShot_f: not enough memory\n");
-		return;
-	}
-	pcx->manufacturer = 0x0a; // PCX id
-	pcx->version = 5; // 256 color
-	pcx->encoding = 1; // uncompressed
-	pcx->bits_per_pixel = 8; // 256 color
-	pcx->xmin = 0;
-	pcx->ymin = 0;
-	pcx->xmax = LittleShort((s16)(width - 1));
-	pcx->ymax = LittleShort((s16)(height - 1));
-	pcx->hres = LittleShort((s16)width);
-	pcx->vres = LittleShort((s16)height);
-	Q_memset(pcx->palette, 0, sizeof(pcx->palette));
-	pcx->color_planes = 1; // chunky image
-	pcx->bytes_per_line = LittleShort((s16)width);
-	pcx->palette_type = LittleShort(2); // not a grey scale
-	Q_memset(pcx->filler, 0, sizeof(pcx->filler));
-	u8 *pack = &pcx->data; // pack the image
-	for (s32 i = 0; i < height; i++) {
-		for (s32 j = 0; j < width; j++) {
-			if ((*data & 0xc0) != 0xc0)
-				*pack++ = *data++;
-			else {
-				*pack++ = 0xc1;
-				*pack++ = *data++;
-			}
-		}
-		data += rowbytes - width;
-	}
-	*pack++ = 0x0c; // palette ID u8
-	for (s32 i = 0; i < 768; i++) // write the palette
-		*pack++ = *palette++;
-	s32 length = pack - (u8 *) pcx; // write output file 
-	COM_WriteFile(filename, pcx, length);
-}
-
-
 void WriteBMPfile(s8 *filename, u8 *data, s32 width, s32 height,
 				  s32 rowbytes, u8 *palette)
 {
